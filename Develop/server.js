2715b5834fbc39de9edc0b4b6e1d9bdd3a370443
hroddy <hannah.roddy@gmail.com>
2021-10-15 16:03:25 -0700

    initial commit with starter code, set up server.js

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT||3001;
+const path = require('path');
+const fs = require('fs');
+
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+
+const apiRouter = require('./routes/apiRoutes');
+const htmlRouter = require('./routes/htmlRoutes');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
+app.use('/api', apiRouter);
+app.use('/', htmlRouter);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port 3001!`);
+});
